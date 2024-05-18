625558a70a6ffd0e4a91c82fbdae5a7f52a7fa60
Yigit <142043726+yeeegit@users.noreply.github.com>
2024-05-19 01:18:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,23 @@ connectDatabase();
 const app = express();
 const port = process.env.PORT;
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://ipss-test-frontend.vercel.app");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
+
 app.use(bodyParser.json({ limit: "15mb" }));
 app.use(bodyParser.urlencoded({ limit: "15mb", extended: true }));
 app.use(cors());
